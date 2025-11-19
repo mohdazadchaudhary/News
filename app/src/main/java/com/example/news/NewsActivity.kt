b61be8d68edf946ea89e7cef27a9dd76442539dd
mohdazadchaudhary <mohdazadchaudhary5@gmail.com>
2025-11-20 03:20:00 +0530

    feat: Setup ViewModel and Bottom Navigation in NewsActivity
    Initializes `NewsViewModel` using a `ViewModelProviderFactory` and a `NewsRepository`.
    
    Implements view binding with `ActivityNewsBinding` to set the content view.
    
    Connects the `BottomNavigationView` with the `NavController` from a `NavHostFragment` for navigation.

--- app/src/main/java/com/example/news/NewsActivity.kt
+++ app/src/main/java/com/example/news/NewsActivity.kt
@@ -2,12 +2,33 @@ package com.example.news
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.news.databinding.ActivityNewsBinding
+import com.example.news.db.ArticleDatabase
+import com.example.news.repository.NewsRepository
+import com.example.news.ui.fragments.NewsViewModelProviderFactory
+import com.example.news.util.NewsViewModel
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+    private lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        }
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+
+        viewModel = ViewModelProvider(this, viewModelProviderFactory)[NewsViewModel::class.java]
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
